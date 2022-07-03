c6bec0d0f739232911ec6e041558def03020f28b
Nikhil Sharma <nikhilsharmapro19@gmail.com>
2022-07-04 01:08:55 +0530

    Private and Public Routes added

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,14 @@
+//eslint-disable-next-line
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+const PrivateRoute = ({ children, ...routeProps }) => {
+  const profile = false;
+
+  if (!profile) {
+    return <Redirect to="/signin" />;
+  }
+  return <Route {...routeProps}>{children}</Route>;
+};
+
+export default PrivateRoute;
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,14 +1,14 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAWjGVCZrA-qRbd-sF-kRl5THaKhllkz3c",
-  authDomain: "chat-web-app-beeda.firebaseapp.com",
-  databaseURL: "https://chat-web-app-beeda-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-web-app-beeda",
-  storageBucket: "chat-web-app-beeda.appspot.com",
-  messagingSenderId: "265862383737",
-  appId: "1:265862383737:web:f651d39e66f976913d2ca8"
+  apiKey: 'AIzaSyAWjGVCZrA-qRbd-sF-kRl5THaKhllkz3c',
+  authDomain: 'chat-web-app-beeda.firebaseapp.com',
+  databaseURL:
+    'https://chat-web-app-beeda-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'chat-web-app-beeda',
+  storageBucket: 'chat-web-app-beeda.appspot.com',
+  messagingSenderId: '265862383737',
+  appId: '1:265862383737:web:f651d39e66f976913d2ca8',
 };
-
 
 const app = initializeApp(firebaseConfig);
