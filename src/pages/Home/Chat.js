867bb3ba2ad98b9de09e51512ac81297abc7a486
Nikhil Sharma <nikhilsharmapro19@gmail.com>
2022-07-11 21:24:55 +0530

    Chat page started creating..

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -1,8 +1,39 @@
 import React from 'react'
+import Top from '../../components/chat-window/top';
+import Messages from '../../components/chat-window/messages';
+import Bottom from '../../components/chat-window/bottom';
+import { useParams } from 'react-router-dom';
+import { Loader } from 'rsuite';
+import { useRooms } from '../../context/room.context';
+
 
 const Chat = () => {
+
+  const {chatId} = useParams();
+  const rooms = useRooms();
+
+  if(!rooms){
+    return <Loader center vertical size="md" content="Loading" speed="slow" />
+  }
+
+  const currentRoom = rooms.find(room => room.id === chatId);
+  if(!currentRoom){
+    return <h6 className='text-center mt-page'>Chat {chatId} not found..</h6>
+  }
+
   return (
-    <div>Chat</div>
+    <div>
+      <div className='chat-top'>
+        <Top />
+      </div>
+      <div  className='chat-middle'>
+        <Messages />
+      </div>
+
+      <div className='chat-bottom'>
+        <Bottom />
+      </div>
+    </div>
   )
 }
 
